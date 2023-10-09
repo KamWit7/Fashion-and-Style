8c426a4e2a0e4f532d6d66cde467ceb405e116a6
Kamil Witkowski <53374963+KamWit7@users.noreply.github.com>
2023-10-09 18:11:56 +0200

    set font size, family, weight

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat'], // 400, 600, 700
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 600,
+        bold: 700,
+      },
+
+      fontSize: {
+        64: ['4rem', '5.6rem'],
+        56: ['3.5rem', '4.2rem'],
+        44: ['2.75rem', '3.85rem'],
+        40: ['2.5rem', '3.5rem'],
+        32: ['2rem', '2.8rem'],
+        24: ['1.5rem', '2.1rem'],
+        20: ['1.25rem', '1.75rem'],
+        18: ['1.125rem', '2.025rem'],
+        16: ['1rem', '1.4rem'],
+        14: ['0.875rem', '1.575rem'],
+        12: ['0.75rem', '1.35rem'],
+      },
+    },
   },
   plugins: [],
 }
