f32d62a99ff41c915f33c51e8a5752d709d8ad8e
KamWit7 <kamilw77@interia.pl>
2024-01-09 21:30:40 +0100

    Add CartForm component and update Basket component

---
+++ src/pages/Basket/CartForm/index.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import ShippingAddress from './Steps/ShippingAddress';
+import ShippingInfo from './Steps/DeliveryOptions';
+import Items from './Steps/Products';
+import Payment from './Steps/Payment';
+
+type StepsTypes = 'items' | 'shippingAddress' | 'deliveryOptions' | 'payment' | 'success' | 'error';
+export type HandleStepsType = { handleSteps: [handleNextStep: () => void, handlePreviousStep: () => void] };
+
+const BasketForm = () => {
+  const [step, setStep] = useState<StepsTypes>('items');
+
+  const handleNextStep = () => {
+    setStep((step) => {
+      if (step === 'items') {
+        return 'shippingAddress';
+      }
+
+      if (step === 'shippingAddress') {
+        return 'deliveryOptions';
+      }
+
+      if (step === 'deliveryOptions') {
+        return 'payment';
+      }
+
+      if (step === 'payment') {
+        return 'success';
+      }
+
+      return 'error';
+    });
+  };
+
+  const handlePreviousStep = () => {
+    setStep((step) => {
+      if (step === 'success') {
+        return 'payment';
+      }
+
+      if (step === 'payment') {
+        return 'deliveryOptions';
+      }
+
+      if (step === 'deliveryOptions') {
+        return 'shippingAddress';
+      }
+
+      if (step === 'shippingAddress') {
+        return 'items';
+      }
+
+      return 'error';
+    });
+  };
+
+  return (
+    <div>
+      {step === 'items' && <Items handleSteps={[handleNextStep, handlePreviousStep]} />}
+      {step === 'shippingAddress' && <ShippingAddress handleSteps={[handleNextStep, handlePreviousStep]} />}
+      {step === 'deliveryOptions' && <ShippingInfo handleSteps={[handleNextStep, handlePreviousStep]} />}
+      {step === 'payment' && <Payment handleSteps={[handleNextStep, handlePreviousStep]} />}
+    </div>
+  );
+};
+
+export default BasketForm;
--- src/pages/Basket/index.tsx
+++ src/pages/Basket/index.tsx
@@ -1,44 +1,7 @@
-import { Await, useLoaderData } from 'react-router-dom';
-import { BasketLoader } from '..';
-import { LoaderData } from '@src/types/loader';
-import ProductDetails from './components/ProductDetlails';
-import { Suspense } from 'react';
-import { useCart } from '@src/context/CardContext/useCart';
-import { isEmpty } from '@utils/isEmpty';
+import CartForm from './CartForm';
 
-const Basket = () => {
-  const loaderCart = useLoaderData() as LoaderData<typeof BasketLoader>;
-  const { cart } = useCart();
-
-  return (
-    <div className="container mx-auto">
-      <table className="min-w-full table-auto">
-        <thead className="bg-gray-800 text-white">
-          <tr>
-            <th className="px-4 py-2">Order Summary</th>
-            <th className="px-4 py-2">Price</th>
-            <th className="px-4 py-2">Quantity</th>
-            <th className="px-4 py-2">Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Suspense fallback={<tr>Products...</tr>}>
-            <Await resolve={loaderCart.cart}>
-              {(loaderCart: API.CardType | null) => {
-                const c = isEmpty(cart) && loaderCart ? loaderCart.items : cart;
-
-                return c.map((item, idx) => (
-                  <tr className="border-b bg-gray-100 text-center" key={idx}>
-                    <ProductDetails {...item} />
-                  </tr>
-                ));
-              }}
-            </Await>
-          </Suspense>
-        </tbody>
-      </table>
-    </div>
-  );
+const Cart = () => {
+  return <CartForm />;
 };
 
-export default Basket;
+export default Cart;
